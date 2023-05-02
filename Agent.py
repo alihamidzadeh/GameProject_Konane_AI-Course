b9b6a7c203fb0d4f529bd510bc7d11ad95ebba41
Ali <ahamidzadeh93@gmail.com>
2023-05-02 16:39:10 +0330

    write Minimax function.

--- Agent.py
+++ Agent.py
@@ -15,7 +15,43 @@ class Agent:
         return move
 
     def max(self, current_board, current_color, depth):
-        NotImplemented
+        if self.game.check_terminal(current_board, current_color):
+            return self.game.evaluate(current_board, current_color, -1000)
+
+        if depth == self.max_depth:
+            return self.game.evaluate(current_board, current_color)
+
+        possible_moves = self.game.generate_all_possible_moves(current_board, current_color)
+        best_move = None
+        best_value = self.MIN_VALUE
+
+        for move in possible_moves:
+            temporary_move, value = self.min(current_board.next_board(current_color, move),
+                                             self.game.opponent(current_color), depth + 1)
+
+            if (value > best_value):
+                best_value = value
+                best_move = temporary_move
+
+        return best_move, best_value
 
     def min(self, current_board, current_color, depth):
-        NotImplemented
+        if self.game.check_terminal(current_board, current_color):
+            return self.game.evaluate(current_board, current_color, -1000)
+
+        if depth == self.max_depth:
+            return self.game.evaluate(current_board, current_color)
+
+        possible_moves = self.game.generate_all_possible_moves(current_board, current_color)
+        best_move = None
+        best_value = self.MAX_VALUE
+
+        for move in possible_moves:
+            temporary_move, value = self.max(current_board.next_board(current_color, move),
+                                             self.game.opponent(current_color), depth + 1)
+
+            if (value < best_value):
+                best_value = value
+                best_move = temporary_move
+
+        return best_move, best_value
